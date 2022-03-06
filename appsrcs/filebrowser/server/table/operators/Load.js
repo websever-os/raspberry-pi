f6d078809d77b4ec8eee5e8a0e83ab447b0f0265
GON HU <gon.hu@outlook.com>
2022-03-07 04:19:59 +0900

    Fix directory loading
     On branch main
     Changes to be committed:
    	modified:   appsrcs/filebrowser/client/table/scripts/js/operators/Load.js
    	modified:   appsrcs/filebrowser/server/table/operators/Load.js

--- appsrcs/filebrowser/server/table/operators/Load.js
+++ appsrcs/filebrowser/server/table/operators/Load.js
@@ -1,12 +1,15 @@
 const path = require('path')
+const fs = require('fs')
 module.exports = function Load(web){
     this.directory = async function(session){
         const app = session.app
         let list = null
-        let dPath = app.data.info.path
-        app.data.info.path = dPath
+        let dPath = path.resolve(app.data.info.path)
         let paths = dPath.split(path.sep)
         paths.shift()
+        console.log(dPath)
+        var state = fs.lstatSync(dPath)
+		if(state.isDirectory())
         if(paths[0] == ''){
             list = []
             list.push(['home',null,'directory',null])
@@ -14,7 +17,7 @@ module.exports = function Load(web){
         }else if(paths[0] == 'home' 
                 || (paths[0] == 'home' && paths[1] == 'pi')
                 || paths[0] == 'media'){
-            list = await web.sys.loads.data.list(app.data.info.path)
+            list = await web.sys.loads.data.list(dPath)
         }
         return list
     }
